8164f9406326440060de385a5ff8522b3eb28cb0
puneetbaviskar@gmail.com <puneetbaviskar@gmail.com>
2021-01-11 22:44:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+// creating my image link
+
+////var link = document.createElement("a");
+//document.body.appendChild(link);
+
+//link.href = "http://punitbaviskar.me/";
+//link.target = "_blank";
+
+//var photo = document.createElement("img");
+//link.appendChild(photo);
+
+//photo.src =
+  //"pb.jpg";
+//photo.alt = "@puneetb22";
+/
+/photo.style =
+ // "border-radius:50%;position:fixed;bottom:20px;right:20px;transition:all 0.5s ease;height:80px;";
+
+//photo.onmouseover = function() {
+  //this.style.transform = "scale(1.1,1.1)";
+  //this.style.boxShadow = "5px 5px 15px #000";
+//};
+
+//photo.onmouseout = function() {
+  //this.style.transform = "scale(1,1)";
+  //this.style.boxShadow = "none";
+//};
